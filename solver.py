40e69324501158fd42041abe699bc53487c5ebd3
Ben Eppinger <beneppinger@gs6160.geos.vt.edu>
2024-04-06 20:55:09 -0400

    Added setpar functionality and deleted unneeded code

--- solver.py
+++ solver.py
@@ -4,131 +4,148 @@ from joblib import Parallel, delayed
 
 class solver:
 
-    def setup(PATHS, PARAMS):
+    def __init__(self, PATHS, PARAMS):
+        self.PATHS = PATHS
+        self.PARAMS = PARAMS
+
+
+    def setup(self):
         """
         Function prepares system to run the solver and other capbilites by initializing the needed directories
         inputs:
-            PATHS: WIPY class containing all important directory locations
-            PARAMS: WIPY class containing all input parameters
+            self: 
         """
+      
+        PATHS = self.PATHS
+        PARAMS = self.PARAMS
 
+        # make basic directory structure
         sp.run(
-            ["rm -r scratch"],
+            ["rm", "-r", "scratch"],
             cwd=PATHS.wipy_root_path,
-            shell=True,
             capture_output=True,
-            text=True
         )
 
         sp.run(
-            ['mkdir OUTPUT'],
+            ['mkdir', 'OUTPUT'],
             cwd=PATHS.wipy_root_path,
-            shell=True,
             capture_output=True,
-            text=True
         )
         
         sp.run(
-            ['mkdir scratch'],
+            ['mkdir', 'scratch'],
             cwd=PATHS.wipy_root_path,
-            shell=True,
             capture_output=True,
-            text=True
         )
 
         sp.run(
-            ['mkdir scratch/solver'],
+            ['mkdir', 'scratch/solver'],
             cwd=PATHS.wipy_root_path,
-            shell=True,
             capture_output=True,
-            text=True
         )
 
+        # make a solver directry for each event with solver executables and data 
         for i in range(PARAMS.n_events):
 
-            dir_name = "{:06d}".format(i)
+            dir_num = "{:06d}".format(i)
 
             SP = sp.run(
-                ["mkdir " + dir_name],
+                ["mkdir", dir_num],
                 cwd=PATHS.scratch_solver_path,
-                shell=True,
                 capture_output=True,
-                text=True
             )
             
             SP = sp.run(
-                ["cp -r " + PATHS.solver_exec_path + " " + "/".join([PATHS.scratch_solver_path, dir_name])], 
+                ["cp", "-r", PATHS.solver_exec_path, "/".join([PATHS.scratch_solver_path, dir_num])], 
                 cwd=PATHS.scratch_solver_path,
-                shell=True,
                 capture_output=True,
-                text=True
             )
             
             SP = sp.run(
-                ["cp -r " + PATHS.solver_data_path + " " + "/".join([PATHS.scratch_solver_path, dir_name])], 
+                ["cp", "-r", PATHS.solver_data_path, "/".join([PATHS.scratch_solver_path, dir_num])], 
                 cwd=PATHS.scratch_solver_path,
-                shell=True,
                 capture_output=True,
-                text=True
+            )
+
+            SP = sp.run(
+                ["cp", "SOURCE_"+dir_num, "SOURCE"],
+                cwd="/".join([PATHS.scratch_solver_path, dir_num, 'DATA']),
+                capture_output=True,
             )
             
             SP = sp.run(
-                ["mkdir OUTPUT_FILES"],
-                cwd="/".join([PATHS.scratch_solver_path, dir_name]),
-                shell=True,
+                ["mkdir", "OUTPUT_FILES"],
+                cwd="/".join([PATHS.scratch_solver_path, dir_num]),
                 capture_output=True,
-                text=True
             )
 
             SP = sp.run(
-                ["mkdir SEM"],
-                cwd="/".join([PATHS.scratch_solver_path, dir_name]),
-                shell=True,
+                ["mkdir", "SEM"],
+                cwd="/".join([PATHS.scratch_solver_path, dir_num]),
                 capture_output=True,
-                text=True
             )
 
-    def call_solver(PATHS, PARAMS, func):
+    
+    def setpar(self, path, par, new_par):
+        """
+        Reads a parameter file and updates it with a new par value.
+        inputs:
+            path: The path the parameter file
+            par: The parameter to be updated
+            new_par: the value for the parameter 
+        """
+
+        with open(path, 'r') as fid: 
+            lines = fid.readlines()
+
+            for i in range(len(lines)):
+                s0 = lines[i].split('=')[0]
+
+                if par in s0 and '#' not in s0:
+                    lines[i]  = par + " = " + new_par + '\n'
+
+        with open(path, 'w') as fid:
+            fid.writelines(lines)
+
+
+    def call_solver(self, func):
         """
         Calls the solver for each event in paralell. Used to call forwards/backwards solvers and to smooth kernels.
         inputs:
-            PATHS: WIPY class containing all important directory locations
-            PARAMS: WIPY class containing all input parameters
+            func: the solver function being called, e.g., forwards, backwards, or smooth. 
         """
-        path_names = ["/".join([PATHS.scratch_solver_path,"{:06d}".format(x)]) for x in range(PARAMS.n_events)]
-        PP = Parallel(n_jobs=PARAMS.n_proc)(delayed(func)(path) for path in path_names)
+        path_names = ["/".join([self.PATHS.scratch_solver_path,"{:06d}".format(x)]) for x in range(self.PARAMS.n_events)]
+        PP = Parallel(n_jobs=self.PARAMS.n_proc)(delayed(func)(path) for path in path_names)
 
 
 class specfem2d(solver):
 
-    def forward(path):
+    def forward(self, path):
         """
         Calls specfem2d forward solver
         inputs:
             path [string]: Will be the root the directory of the specfem call. It should contain the DATA, OUTPUT_FILES, bin, and SEM directories. 
         """
+        super().setpar(
+            path="/".join([path, 'DATA', 'Par_file']),
+            par='SIMULATION_TYPE',
+            new_par='1'
+        )
         
-        #
-        # Add line here to make sure that the simulation_type = 1
-        #
-
         SP = sp.run(
             ["./bin/xmeshfem2D"],
             cwd=path,
-            shell=True,
             capture_output=True,
-            text=True
         )
         
         SP = sp.run(
             ["./bin/xspecfem2D"],
             cwd=path,
-            shell=True,
             capture_output=True,
-            text=True
         )
 
-    def adjoint(path):
+    
+    def adjoint(self, path):
         """
         Calls specfem2d adjoint solver
         inputs:
@@ -142,15 +159,15 @@ class specfem2d(solver):
         SP = sp.run(
             ["./bin/xspecfem2D"],
             cwd=path,
-            shell=True,
             capture_output=True,
-            text=True
         )
     
-    def smooth(path):
+    
+    def smooth(self, path):
         return None
     
-    def sum_kernels(path_list):
+    
+    def sum_kernels(self, path_list):
         return None
 
 
