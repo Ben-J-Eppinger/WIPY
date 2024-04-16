3ac04f210803c2457180ce51567eeeb74f6bf119
Ben Eppinger <beneppinger@gs6160.geos.vt.edu>
2024-04-16 18:17:49 -0400

    added preprocess_test workflow

---
+++ wipy/work_flows/preprocess_test.py
@@ -0,0 +1,29 @@
+from wipy.base import base, paths, params
+
+# initialize the wipy paths and params classes (these will read your input files)
+PATHS = paths()
+PARAMS = params()
+
+# create a wipy base object using the base class and run the clean and setup methods
+b = base(PATHS, PARAMS)
+b.clean()
+b.setup()
+
+# import the solver specifieid in the parameters.py file 
+_temp = __import__(".".join(["wipy", "solver", PARAMS.solver]), fromlist=PARAMS.solver)
+solver_class = getattr(_temp, PARAMS.solver)
+
+# initialize a solver object and use it to do forward modeling
+s = solver_class(PATHS, PARAMS)
+s.call_solver(s.forward)
+s.export_traces()
+
+# import preprocess test, initialize preprocessor class, and use it to preprocess data
+from wipy.preprocess.preprocess_base import preprocess_base
+p = preprocess_base(PATHS, PARAMS)
+p.call_preprocessor(data_type='obs')
+p.call_preprocessor(data_type='syn')
+
+# save traces if desired
+if PARAMS.save_traces:
+    s.save_traces()
